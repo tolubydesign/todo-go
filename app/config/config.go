6712e0a26769179ac0f335bf5d357ae7161da6b3
Tolu Adesina <tolu.adesina79@gmail.com>
2025-09-30 02:15:34 +0200

    chore: add env variables to communicate with database

--- app/config/config.go
+++ app/config/config.go
@@ -4,13 +4,23 @@ import (
 	"errors"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
+type MySqlDatabase struct {
+	SqlHost     string `json:"sqlHost"`
+	SqlPort     int    `json:"sqlPort"`
+	SqlUser     string `json:"sqlUser"`
+	SqlPassword string `json:"sqlPassword"`
+	SqlDatabase string `json:"sqlDatabase"`
+}
+
 type Configuration struct {
-	Environment string `json:"environment"`
-	Port        string `json:"port"`
+	Environment string        `json:"environment"`
+	Port        string        `json:"port"`
+	Mysql       MySqlDatabase `json:"mysql"`
 }
 
 type Config struct {
@@ -33,10 +43,31 @@ func BuildConfiguration() (*Config, error) {
 
 	port := os.Getenv("PORT")
 	environment := os.Getenv("ENV")
+	sqlHost := os.Getenv("MYSQL_HOST")
+	sqlPort := os.Getenv("MYSQL_PORT")
+	sqlUser := os.Getenv("MYSQL_USER")
+	sqlPassword := os.Getenv("MYSQL_PASSWORD")
+	sqlDatabase := os.Getenv("MYSQL_DATABASE")
+	intSqlPort, err := strconv.Atoi(sqlPort)
+	if err != nil {
+		log.Println("Error converting string to int", "warn")
+		// log.Fatalf("Error converting string to int: %v", err)
+	}
+	// TODO: add additional errors to make sure all required environment values are provided
+
+	database := MySqlDatabase{
+		SqlHost:     sqlHost,
+		SqlPort:     intSqlPort,
+		SqlUser:     sqlUser,
+		SqlPassword: sqlPassword,
+		SqlDatabase: sqlDatabase,
+	}
+
 	configurationSingleton = &Config{
 		Configuration: &Configuration{
 			Environment: environment,
 			Port:        port,
+			Mysql:       database,
 		},
 	}
 
